f65925e7475c2f5554b63fddd65946e0433667ab
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-10-07 06:59:51 +0530

    Create my-cal-iii.cpp

---
+++ oct-22/my-cal-iii.cpp
@@ -0,0 +1,20 @@
+// 07 Oct 2022
+// 732. My Calendar III
+
+
+class MyCalendarThree {
+public:
+    MyCalendarThree() {
+        
+    }
+    
+     map<int, int> count = {{-1, 0}};
+    int res = 0;
+    int book(int s, int e) {
+        auto start = count.emplace(s, (--count.upper_bound(s))->second);
+        auto end = count.emplace(e, (--count.upper_bound(e))->second);
+        for (auto i = start.first; i != end.first; ++i)
+            res = max(res, ++(i->second));
+        return res;
+    }
+};
