200735071c81556b422b8899dec004cf8139099f
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-10-19 05:40:06 +0530

    Create top-k-frequency-words.cpp

---
+++ top-k-frequency-words.cpp
@@ -0,0 +1,13 @@
+// 19 Oct 2022
+// 692. Top K Frequent Words
+
+
+def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        dict = {}
+        for x in words:
+            if x in dict:
+                dict[x] += 1
+            else:
+                dict[x] = 1
+        res = sorted(dict, key=lambda x: (-dict[x], x))
+        return res[:k]
