7b0313093d85f20882562a4616b6cf1c85601184
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-10-23 08:34:49 +0530

    Create set-mismatch.py

---
+++ oct-22/set-mismatch.py
@@ -0,0 +1,15 @@
+// 23 Oct 2022
+// 645. Set Mismatch
+
+
+class Solution(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        s = n*(n+1)//2
+        miss = s - sum(set(nums))
+        duplicate = sum(nums) + miss - s
+        return [duplicate, miss]
