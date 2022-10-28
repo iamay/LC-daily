58987cc7836cdc391718eaff9af0e3680e984571
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-10-28 19:35:41 +0530

    Create group-anagram.py

---
+++ oct-22/group-anagram.py
@@ -0,0 +1,13 @@
+// 28 Oct 2022
+// 49. group anagram
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
