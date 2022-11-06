12dfc6aec62334f005ba9d38ac60904adc383fd6
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-11-06 07:18:54 +0530

    Create orderly-queue.cpp

---
+++ nov-22/orderly-queue.cpp
@@ -0,0 +1,17 @@
+// 06 Nov 2022
+// 899. Orderly Queue
+
+
+class Solution {
+public:
+      string orderlyQueue(string S, int K) {
+        if (K > 1) {
+            sort(S.begin(), S.end());
+            return S;
+        }
+        string res = S;
+        for (int i = 1; i < S.length(); i++)
+            res = min(res, S.substr(i) + S.substr(0, i));
+        return res;
+    }
+};
