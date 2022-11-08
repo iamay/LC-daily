26ae96ab230a10949ab47fb05b082ae8779d198f
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-11-08 10:06:41 +0530

    Create make-string-great.cpp

---
+++ nov-22/make-string-great.cpp
@@ -0,0 +1,15 @@
+// 08 Nov 2022
+// 1544. Make The String Great
+
+class Solution {
+public:
+    string makeGood(string s, int p = 0) {
+    for (int i = 0; i < s.size(); ++i) {
+        if (p > 0 && abs(s[i] - s[p - 1]) == 32)
+            --p;
+        else
+            s[p++] = s[i];
+    }
+    return s.substr(0, p);
+}
+};
