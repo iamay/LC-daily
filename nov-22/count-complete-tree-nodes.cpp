ce51fbd687dd677a8a737478438abcc6fbbe5316
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-11-15 07:24:54 +0530

    Create count-complete-tree-nodes.cpp

---
+++ nov-22/count-complete-tree-nodes.cpp
@@ -0,0 +1,25 @@
+// 15 Nov2022
+// 222. Count Complete Tree Nodes
+class Solution {
+
+public:
+
+    int countNodes(TreeNode* root) {
+
+        if(!root) return 0;
+
+        int hl=0, hr=0;
+
+        TreeNode *l=root, *r=root;
+
+        while(l) {hl++;l=l->left;}
+
+        while(r) {hr++;r=r->right;}
+
+        if(hl==hr) return pow(2,hl)-1;
+
+        return 1+countNodes(root->left)+countNodes(root->right);
+
+    }
+
+};
