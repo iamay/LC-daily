cd7621aabe04be77e579617db4fe4ee40f3825e9
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-12-14 06:33:43 +0530

    Create house-robber.cpp

---
+++ dec-22/house-robber.cpp
@@ -0,0 +1,15 @@
+// 14  Dec 2022
+// 198. House Robber
+
+class Solution {
+public:
+    int rob(vector<int>& nums) { 
+        int n = nums.size(), pre = 0, cur = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = max(pre + nums[i], cur);
+            pre = cur;
+            cur = temp;
+        }
+        return cur;
+    }
+};
