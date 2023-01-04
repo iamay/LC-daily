9a76b06fde5d6e3be0d79e37783da920145d683f
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-01-04 08:11:30 +0530

    Create min-round-to-complete-all-tasks.cpp

---
+++ jan-23/min-round-to-complete-all-tasks.cpp
@@ -0,0 +1,25 @@
+// 04 Jan 2023
+// 2244. Minimum Rounds to Complete All Tasks
+
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        map<int,int> hm;
+        for(int i=0;i<tasks.size();i++){
+            hm[tasks[i]]++;
+        }
+        int num,freq,ans=0;
+        for (auto i : hm){
+            freq = i.second;
+            if(freq==1)
+                return -1;
+            if(freq%3==0){
+                ans += freq/3;
+            }
+            else{
+                ans += freq/3 + 1;
+            }
+        }
+        return ans;
+    }
+};
