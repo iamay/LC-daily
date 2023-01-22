b2ba4ada9650024a6ca0a1a6d121bb8b63e63207
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-01-22 08:13:38 +0530

    Create palindrome-partitioning.py

---
+++ jan-23/palindrome-partitioning.py
@@ -0,0 +1,13 @@
+// 22 Jan 2023
+// 131. Palindrome Partitioning
+
+class Solution(object):
+    @cache  # the memory trick can save some time
+    def partition(self, s):
+        if not s: return [[]]
+        ans = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:  # prefix is a palindrome
+                for suf in self.partition(s[i:]):  # process suffix recursively
+                    ans.append([s[:i]] + suf)
+        return ans
