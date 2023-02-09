b8bea27f8314f5e1332272cbba1282f5243be70d
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-02-09 07:59:44 +0530

    Create naming-a-company.py

---
+++ feb-23/naming-a-company.py
@@ -0,0 +1,15 @@
+// 09 Feb 2023
+// 2306. Naming a Company
+
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
+            A = [set() for _ in range(26)]
+            for idea in ideas:
+                A[ord(idea[0]) - ord('a')].add(idea[1:])
+            
+            ans = 0
+            for i in range(25):
+                for j in range(i + 1, 26):
+                    k = len(A[i] & A[j]) 
+                    ans += 2 * (len(A[i]) - k) * (len(A[j]) - k)
+            return ans
