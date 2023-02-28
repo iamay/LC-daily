af08a32cc8ebd7b4761727b1876fa3fe7e23d580
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-02-28 21:50:54 +0530

    Create find-duplicate-subtrees.py

---
+++ feb-23/find-duplicate-subtrees.py
@@ -0,0 +1,13 @@
+// 28 Feb 2023
+// 652. Find Duplicate Subtrees
+
+def findDuplicateSubtrees(self, root):
+        def trv(root):
+            if not root: return "null"
+            struct = "%s,%s,%s" % (str(root.val), trv(root.left), trv(root.right))
+            nodes[struct].append(root)
+            return struct
+        
+        nodes = collections.defaultdict(list)
+        trv(root)
+        return [nodes[struct][0] for struct in nodes if len(nodes[struct]) > 1]
