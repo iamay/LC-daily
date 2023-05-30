48e4025bfc7a33891c5202f5d18d60ac7b8a6e32
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-05-30 06:02:38 +0530

    Create design-hashset.js

---
+++ may-23/design-hashset.js
@@ -0,0 +1,38 @@
+// 30 May 2023
+// 705. Design HashSet
+
+var MyHashSet = function() {
+     this.hashSet={}
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    this.hashSet[key]=key
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+     delete this.hashSet[key]
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return key in this.hashSet
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
