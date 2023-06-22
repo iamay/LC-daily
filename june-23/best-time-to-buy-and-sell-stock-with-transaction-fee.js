a9a82407e4501e7f34adf398b5c6e32633080ac5
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-06-22 07:18:43 +0530

    Create best-time-to-buy-and-sell-stock-with-transaction-fee.js

---
+++ june-23/best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -0,0 +1,15 @@
+// 22 June 2023
+// 714. Best Time to Buy and Sell Stock with Transaction Fee
+
+var maxProfit = function(prices, fee) {
+    if(prices.length < 2) return 0;
+    let profit = 0, min = prices[0];
+    for(let g=1; g<prices.length; g++){
+        if( prices[g] < min ) min = prices[g];
+        else if( prices[g] > min +  fee ) {
+           profit = profit +  prices[g] - fee - min;
+           min = prices[g] - fee;
+        }
+    }
+    return profit;    
+};
