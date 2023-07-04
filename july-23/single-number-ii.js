7890dc0e6c603f278722ffaa0d043963a6d32f8e
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-07-04 06:51:35 +0530

    Create single-number-ii.js

---
+++ july-23/single-number-ii.js
@@ -0,0 +1,22 @@
+// 04 July 2023
+// 137. Single Number II
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+
+    let ones = 0
+    let twos = 0
+
+    for(let num of nums){
+        twos |= ones & num
+        ones ^= num
+
+        threes = ones & twos
+        ones &= ~threes
+        twos &= ~threes
+    }
+    return ones
+};
