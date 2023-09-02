e68c009b5b53284d9168adfc7ccd36cd2800956f
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-09-02 06:51:59 +0530

    Create extra-char-in-a-str.js

---
+++ sept-23/extra-char-in-a-str.js
@@ -0,0 +1,19 @@
+// 02 September 2023
+// 2707. Extra Characters in a String
+
+var minExtraChar = function(s, dictionary) {
+    const n = s.length;
+    const dp = new Array(n + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;
+
+        for (const word of dictionary) {
+            if (i >= word.length && s.slice(i - word.length, i) === word) {
+                dp[i] = Math.min(dp[i], dp[i - word.length]);
+            }
+        }
+    }
+
+    return dp[n];
+};
