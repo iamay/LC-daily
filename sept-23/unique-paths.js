98a11caa6d2d13f85a825551ed66ed7eebd8b94d
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-09-03 08:24:12 +0530

    Create unique-paths.js

---
+++ sept-23/unique-paths.js
@@ -0,0 +1,18 @@
+// 03 September 2023
+// 62. Unique Paths
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const memo = new Array(m).fill(1).map(() => new Array(n).fill(1));
+    
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+        }
+    }
+
+    return memo[m - 1][n - 1];
+};
