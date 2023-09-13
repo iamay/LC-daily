90233378e980553d453417707149c9726c20bccf
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-09-13 08:28:13 +0530

    Create candy.js

---
+++ sept-23/candy.js
@@ -0,0 +1,23 @@
+// 13 Sept 2023
+// 135. Candy
+var candy = function(ratings) {
+    var candies = [], i;
+    var currentNum = 1;
+    candies[0] = 1; // set first value to 1
+    for(i = 1; i < ratings.length; i++){
+		/* set value to one greater if greater than previous value, otherwise to 1 */
+        if (ratings[i] > ratings[i-1]) currentNum++;
+        else currentNum = 1;
+        candies[i] = currentNum;
+    }
+    for(i = ratings.length - 2; i >= 0; i--){
+		/* set value to max of one greater than previous or to current value */
+        if (ratings[i] > ratings[i+1]) currentNum++;
+        else currentNum = 1;
+        candies[i] = Math.max(currentNum, candies[i]);
+    }
+	/* return sum of all candies array */
+    return candies.reduce(function(total, value){
+        return total + value;
+    });
+};
