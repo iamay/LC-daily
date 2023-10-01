4499d0849f5eeef8dad84523be2246bf510a5af6
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-10-01 08:56:43 +0530

    Create reverse-word-in-str-iii.js

---
+++ oct-23/reverse-word-in-str-iii.js
@@ -0,0 +1,15 @@
+// 01 Oct 2023
+// 557. Reverse Words in a String III
+var reverseWords = function(s) {
+    let res = '';
+    let word = '';
+    for (let c of s) {
+        if (c === ' ') {
+            res += word + c;
+            word = '';
+        } else {
+            word = c + word;
+        }
+    }
+    return res + word;
+};
