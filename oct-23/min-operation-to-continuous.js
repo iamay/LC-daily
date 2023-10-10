acff700c640aedb01e2ea30e5e1f70d07285a91e
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-10-10 07:34:00 +0530

    Create min-operation-to-continuous.js

---
+++ oct-23/min-operation-to-continuous.js
@@ -0,0 +1,14 @@
+// 10  Oct 2023
+// 2009. Minimum Number of Operations to Make Array Continuous
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+    let hash = [...new Set(nums)].sort(((a,b)=>a-b)), count = 0, arr = []
+    for ( let n of hash ) {
+        while ( n-arr[0] >= nums.length ) arr.shift()
+        count = Math.max( arr.push(n), count )
+    }
+    return nums.length-count
+};
