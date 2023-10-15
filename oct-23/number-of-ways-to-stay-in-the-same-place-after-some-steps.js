18d88f39c79e4efe734a6d651206b1ddc842c558
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-10-15 08:03:38 +0530

    Create number-of-ways-to-stay-in-the-same-place-after-some-steps.js

---
+++ oct-23/number-of-ways-to-stay-in-the-same-place-after-some-steps.js
@@ -0,0 +1,31 @@
+// 15 Oct 2023
+// 1269. Number of Ways to Stay in the Same Place After Some Steps
+/**
+ * @param {number} steps
+ * @param {number} arrLen
+ * @return {number}
+ */
+var numWays = function(steps, arrLen) {
+  if (arrLen === 1) {
+    return 1
+  }
+  let preRecord = Array(arrLen).fill(0)
+  let curRecord = Array(arrLen)
+  preRecord[0] = 1
+  const mod = 10 ** 9 + 7
+  // const limit = Math.min(steps, arrLen)
+  // for (let step = 1; step <= limit; step++) {
+  for (let step = 1; step <= steps; step++) {
+    for (let i = 0; i < steps; i++) {
+      if (i === 0) {
+        curRecord[i] = (preRecord[i] + preRecord[i + 1]) % mod
+      } else if (i === arrLen - 1) {
+        curRecord[i] = (preRecord[i - 1] + preRecord[i]) % mod
+      } else {
+        curRecord[i] = (preRecord[i - 1] + preRecord[i] + preRecord[i + 1]) % mod
+      }
+    }
+    preRecord = curRecord.slice()
+  }
+  return curRecord[0]
+};
