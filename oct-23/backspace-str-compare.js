6563e5d30cca74df410d97b7772b14a9e8e83581
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-10-19 16:00:54 +0530

    Create backspace-str-compare.js

---
+++ oct-23/backspace-str-compare.js
@@ -0,0 +1,24 @@
+// 19 Oct 2023
+// 844. Backspace String Compare
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceString = (string) => {
+    let output = [];
+    
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "#") {
+            output.pop();
+        } else {
+            output.push(string[i]);
+        }
+    }
+
+    return output.join('');
+}
+
+var backspaceCompare = function(s, t) {
+    return backspaceString(s) === backspaceString(t);
+};
