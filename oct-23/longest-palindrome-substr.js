58bd5f55085a38e377186874ce20fb83910d2b46
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-10-27 07:02:53 +0530

    Create longest-palindrome-substr.js

---
+++ oct-23/longest-palindrome-substr.js
@@ -0,0 +1,26 @@
+// 27 Oct 2023
+// 5. Longest Palindromic Substring
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = function (s) {
+	const n = s.length,
+		memo = new Array(n).fill().map((v) => new Array(n));
+
+	const dp = (i, j) => {
+		if (i >= j) return true;
+		if (memo[i][j] != undefined) return memo[i][j];
+		memo[i][j] = s[i] != s[j] ? false : dp(i + 1, j - 1);
+		return memo[i][j];
+	};
+
+	let length = s.length - 1;
+	while (length > 0) {
+		for (let i = 0; i + length < s.length; i++)
+			if (dp(i, i + length)) return s.slice(i, i + length + 1);
+		length--;
+	}
+
+	return s[0];
+};
