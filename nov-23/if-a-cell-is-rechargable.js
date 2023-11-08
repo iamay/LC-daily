c7ff7192e354afc5bc2a06988dfdec45530049e5
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2023-11-08 08:05:07 +0530

    Create if-a-cell-is-rechargable.js

---
+++ nov-23/if-a-cell-is-rechargable.js
@@ -0,0 +1,24 @@
+// 08 Nov 2023
+// 2849. Determine if a Cell Is Reachable at a Given Time
+/**
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} fx
+ * @param {number} fy
+ * @param {number} t
+ * @return {boolean}
+ */
+var isReachableAtTime = function(sx, sy, fx, fy, t) {
+    
+    let distx =Math.abs( sx-fx)
+    let disty = Math.abs(sy-fy)
+    if(distx==0 && disty==0 &&t==1){
+        return false
+    }
+    let timeneeded = Math.min(distx,disty)
+    distx-=timeneeded
+    disty-=timeneeded
+    
+    timeneeded+=Math.max(distx,disty)
+    return timeneeded<=t
+};
